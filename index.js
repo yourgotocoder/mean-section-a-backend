c41df558d4e89ec70aa5b7478d04c534e382a133
yourgotocoder <yourgotocoder@gmail.com>
2022-11-01 10:39:22 +0530

    Create a post handler

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 //Import your express package
 const express = require("express");
-
+//Import mongodb driver
+const { MongoClient } = require("mongodb");
 //Initialize one express instance
 const server = express();
 
 //Add one route for the server to listen to
 server.use(express.json());
 
-server.post("/add-todo", (req, res) => {
+server.post("/add-todo", async (req, res) => {
     const { title } = req.body;
+    const client = new MongoClient("mongodb+srv://cse:csesmit123@cluster0.udrw5uh.mongodb.net/?retryWrites=true&w=majority");
     console.log(title);
+    await client.connect();
+    const db = client.db("todo");
+    const collection = db.collection("todo-items");
+    await collection.insertOne({title: title});
     res.json({message: "Success"})
 })
 
